5b3e7990dcbf5a3d797e52cc60ced91747d8cf07
sammuel clark <sammuelclark@gmail.com>
2015-06-02 00:43:10 -0400

    this struggle

--- src/karell/Tester.java
+++ src/karell/Tester.java
@@ -1,25 +1,28 @@
 package karell;
 
-
-//this is neck
-	// sigh......
-		//Sam will learn the struggle of coding
-
 	import kareltherobot.*;
 
 	// This robot task is from page 11 in the text
-	public class Tesster implements Directions
-	{
-
-	    public static void main(String[] args)
-	    {
-	        // World file (leave out the readWorld line if you just want a blank world)
-	        World.readWorld("WorldFiles/fig2-9.kwld");
-	        World.setVisible(true);
-	        
-	        UrRobot karel = new UrRobot(1,2,East,1);
-	        // Your instructions to complete this task should 
-	    }
+	public class Tester implements Directions
+{
+		public void turnRight()
+		{
+				turnLeft();
+				turnLeft();
+				turnLeft();
+		}
+		    public static void main(String[] args)
+		    {
+		        // World file (leave out the readWorld line if you just want a blank world)
+		        World.readWorld("WorldFiles/fig2-9.kwld");
+		        World.setVisible(true);
+		        
+		        UrRobot karel = new UrRobot(1,2,East,1);
+		        // Your instructions to complete this task should 
+		        karel.move();
+		        karel.turnLeft();
+		        karel.turnLeft();
+		    }
 
 
 }
